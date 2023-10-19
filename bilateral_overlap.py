c809e3d4201429a635e885ffe85409abe553a0cf
amitv <amit.vigodman@kenshoo.com>
2023-10-19 18:46:08 +0300

    add remarks

--- bilateral_overlap.py
+++ bilateral_overlap.py
@@ -1,22 +1,31 @@
 from os.path import basename, join
+
 import arcpy
 
 
 def find_bilateral_overlap(fc1, fc2, output_source):
+    # Enable overwriting of output data
     arcpy.env.overwriteOutput = True
 
-    intersect_features = arcpy.Intersect_analysis([fc1, fc2], join(output_source, 'intersect_features'),
-                                                  "ONLY_FID")
+    # Step 1: Perform an intersection of two input feature classes (fc1 and fc2)
+    intersect_features = arcpy.Intersect_analysis([fc1, fc2], join(output_source, 'intersect_features'), "ONLY_FID")
+
+    # Create a list of feature classes (fc1 and fc2)
     feature_classes = [fc1, fc2]
+
+    # Step 2: Loop through the feature classes and create tables for each
     for fc in feature_classes:
+        # Create a table for the intersection of each feature class
         table = arcpy.analysis.TabulateIntersection(fc, "OBJECTID", intersect_features,
                                                     join(output_source, f"{fc1.lower()}_table"), "OBJECTID")
 
-        # Join and rename fields
+        # Step 3: Join and rename fields
+        # Join the table with the intersection results based on OBJECTID
         arcpy.management.JoinField(intersect_features, "OBJECTID", table, "OBJECTID_12", ["PERCENTAGE"])
-        arcpy.management.AlterField(intersect_features, "PERCENTAGE", f"PERCENTAGE_{feature_classes.index(fc)+1}",
-                                    f"PERCENTAGE_{feature_classes.index(fc)+1}")
 
+        # Rename the PERCENTAGE field to distinguish between feature classes
+        arcpy.management.AlterField(intersect_features, "PERCENTAGE", f"PERCENTAGE_{feature_classes.index(fc) + 1}",
+                                    f"PERCENTAGE_{feature_classes.index(fc) + 1}")
+
+    # Step 4: Return the intersected features
     return intersect_features
-
-
--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import arcpy
 from bilateral_overlap import find_bilateral_overlap
 
 
+# Class to store parameter values
 class Params:
     def __init__(self):
+        # Set the paths to input feature classes and workspace
         self.moria_buildings = r'C:\Users\amitv\Desktop\TONOA\bordered_data.gdb\B_BUILDINGS_A'
         self.bs_buildings = r'C:\Users\amitv\Desktop\TONOA\bordered_data.gdb\no_dupes_leb_new_new'
         self.workspace_source = r'C:\Users\amitv\Desktop\TONOA\workspace.gdb'
         self.target_source = r'C:\Users\amitv\Desktop\TONOA\target.gdb'
 
 
+# Abstract base class for finding features
 class FindFeatures(Params):
     def __init__(self):
-        super().__init__()
+        super().__init()
         self.fc_name = None
         self.selection = None
+        # Create feature layers for the input feature classes
         self.bs_buildings_layer = arcpy.MakeFeatureLayer_management(self.bs_buildings, "bs_buildings_lyr")
         self.moria_buildings_layer = arcpy.MakeFeatureLayer_management(self.moria_buildings, "moria_buildings_lyr")
 
@@ -30,41 +34,50 @@ class FindFeatures(Params):
         if not match_count:
             print(f'There aren\'t any {self.fc_name}')
         else:
+            # Create a new feature class based on the selected features
             arcpy.FeatureClassToFeatureClass_conversion(self.bs_buildings_layer,
                                                         self.target_source, self.fc_name)
 
 
+# Class for finding renewing features
 class FindRenewingFeatures(FindFeatures, ABC):
     def __init__(self):
-        super().__init__()
+        super().__init()
         self.fc_name = 'renewing_features'
 
     def find_features(self):
+        # Select features that intersect with another feature layer
         self.selection = arcpy.management.SelectLayerByLocation(self.bs_buildings_layer, "INTERSECT",
                                                                 self.moria_buildings_layer, None, "NEW_SELECTION",
                                                                 "INVERT")
 
 
+# Class for finding features with overlap percentage
 class FindFeaturesWithOverlapPercentage(FindFeatures, ABC):
     def __init__(self, bilateral_overlap):
-        super().__init__()
+        super().__init()
         self.query = None
         self.bilateral_overlap = bilateral_overlap
-        self.bilateral_overlap_layer = arcpy.MakeFeatureLayer_management(self.bilateral_overlap, "bilateral_overlap_lyr")
+        # Create a feature layer for bilateral overlap data
+        self.bilateral_overlap_layer = arcpy.MakeFeatureLayer_management(self.bilateral_overlap,
+                                                                         "bilateral_overlap_lyr")
 
     def find_features(self):
         arcpy.env.overwriteOutput = True
 
+        # Select features based on a query and create a new feature class
         selection = arcpy.management.SelectLayerByAttribute(self.bilateral_overlap_layer, "NEW_SELECTION", self.query)
         match_count = int(arcpy.management.GetCount(selection)[0])
         if match_count:
             selection_layer = arcpy.FeatureClassToFeatureClass_conversion(self.bilateral_overlap_layer,
                                                                           self.workspace_source,
                                                                           'bilateral_overlap_selection_lyr')
+            # Select features that intersect with the newly created selection layer
             self.selection = arcpy.management.SelectLayerByLocation(self.bs_buildings_layer,
                                                                     "INTERSECT", selection_layer)
 
 
+# Class for finding replacing features
 class FindReplacingFeatures(FindFeaturesWithOverlapPercentage, ABC):
     def __init__(self, bilateral_overlap):
         super().__init__(bilateral_overlap)
@@ -72,6 +85,7 @@ class FindReplacingFeatures(FindFeaturesWithOverlapPercentage, ABC):
         self.query = "PERCENTAGE_1 >= 70 And PERCENTAGE_2 >= 70"
 
 
+# Class for finding deleting features
 class FindDeletingFeatures(FindFeaturesWithOverlapPercentage, ABC):
     def __init__(self, bilateral_overlap):
         super().__init__(bilateral_overlap)
@@ -79,10 +93,13 @@ class FindDeletingFeatures(FindFeaturesWithOverlapPercentage, ABC):
         self.query = "PERCENTAGE_1 <= 10 And PERCENTAGE_2 <= 10"
 
 
-if __name__ == '__main__':
+if __name__ == '__main':
     params = Params()
+
+    # Find bilateral overlap features by calling the `find_bilateral_overlap` function
     bilateral_overlap = find_bilateral_overlap(params.moria_buildings,
                                                params.bs_buildings, params.workspace_source)
+
     # Renewing features
     renewing_features = FindRenewingFeatures()
     renewing_features.find_features()
